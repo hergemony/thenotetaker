4af588d8d56e076ec015670d8691cf02cfa59caf
Chi Doan <chi.doan86@gmail.com>
2022-05-05 14:48:19 +0800

    web js

---
+++ routing/web.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    const indexHtmlPath = path.join(__dirname, "public", "index.html");
+
+    res.sendFile(indexHtmlPath);
+});
+
+router.get("/notes", (req, res) => {
+    const notesHtmlPath = path.join(__dirname, "public", "notes.html");
+
+    res.sendFile(notesHtmlPath);
+});
+
+module.exports = router;
