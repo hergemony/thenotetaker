5e2d6a8b818b2a41b9358c0e951cf4aadd89c74a
Chi Doan <chi.doan86@gmail.com>
2022-05-05 13:51:06 +0800

    config

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,1 +1,23 @@
-// to develop using Express js
+// to develop using Express js and create the backend with server.js
+
+const express = require('express');
+
+const app = express();
+
+//configure the Port
+const PORT = 3001;
+
+// send request and get response
+
+app.get('/',(req,res) => {
+    
+    const indexHtmlPath = path.join(__dirname, 'public', 'index.html');
+
+    res.sendFile(indexHtmlPath)
+
+})
+
+app.listen(PORT, function(){
+    console.log(`App is running on http://localhost:${PORT}`);
+});
+
