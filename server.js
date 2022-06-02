ba13849b765e7da54714eceb1ebf540509ad5ebb
Chi Doan <chi.doan86@gmail.com>
2022-06-02 16:29:51 +0800

    fix packagejson

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const apifile = require('./routing/api');
 const app = express();
 
 //configure the Port
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use (express.json())
 app.use( express.static('public') );
